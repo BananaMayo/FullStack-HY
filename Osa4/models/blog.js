2e0e60dbaf1b74e8f80d94b29ee22f41562b9669
BananaMayo <niklas.nygard01@gmail.com>
2023-04-13 11:54:31 +0300

    tehtävä 4.2

--- Osa4/models/blog.js
+++ Osa4/models/blog.js
@@ -8,8 +8,12 @@ const blogSchema = mongoose.Schema({
 })
 
 
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl)
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
 
 module.exports = mongoose.model('Blog', blogSchema)
